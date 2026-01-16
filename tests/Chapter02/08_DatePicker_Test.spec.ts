aaf5708b0c170b58ef4976d999aa5fae56a67987
hassane08433-wq <hassane08433@gmail.com>
2026-01-16 08:40:42 +0500

    created date picker code

---
+++ tests/Chapter02/08_DatePicker_Test.spec.ts
@@ -0,0 +1,29 @@
+// import playwright module
+import { test, expect } from '@playwright/test';
+
+//write a test
+test('Selecting date value in Playwright', async ({ page }) => {
+
+
+    //goto url
+    await page.goto('https://jqueryui.com/datepicker/');
+    //hardcoded date
+    const iframe = page.frameLocator('[class="demo-frame"]');
+    // await iframe.locator('[id="datepicker"]').fill('12/15/2024');
+
+    //selecting dynamic date
+    //await iframe.locator('[id="datepicker"]').click();
+    //await iframe.locator('.ui-datepicker-today').click();
+
+    //selecting past date
+    // await iframe.locator('[id="datepicker"]').click();
+    // await iframe.locator('[title="Prev"]').click();
+    // await iframe.locator('text="15"').click();
+
+    //selecting future date
+    await iframe.locator('[id="datepicker"]').click();
+    await iframe.locator('[title="Next"]').click();
+    await iframe.locator('text="15"').click();
+
+
+});
