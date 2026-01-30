73b009e501b997c6b3a69eb6ff26b9088ae482b4
hassane08433-wq <hassane08433@gmail.com>
2026-01-30 23:25:43 +0500

    annotations

---
+++ tests/Chapter03/01_Annotations_Test.spec.ts
@@ -0,0 +1,63 @@
+// import playwright module
+import { test, expect } from '@playwright/test';
+
+//write a test
+test('Test1', async ({ page }) => {
+
+    console.log('Test Execution Started..')
+    //goto url
+    await page.goto('https://github.com/');
+
+    //click signin
+    await page.getByRole('link', { name: 'Sign in' }).click();
+
+    //enter username & password
+    await page.getByRole('textbox', { name: 'Username or email address' }).fill('testerstalk');
+    await page.getByRole('textbox', { name: 'Username or email address' }).press('Tab');
+    await page.getByRole('textbox', { name: 'Password' }).fill('tester123');
+
+    //enter sign in
+    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+    await expect(page.getByText('Incorrect username or')).toHaveText('Incorrect username or password.');
+
+})
+
+test.skip('Test2', async ({ page }) => {
+
+    console.log('Test Execution Started..')
+    //goto url
+    await page.goto('https://github.com/');
+
+    //click signin
+    await page.getByRole('link', { name: 'Sign in' }).click();
+
+    //enter username & password
+    await page.getByRole('textbox', { name: 'Username or email address' }).fill('testerstalk');
+    await page.getByRole('textbox', { name: 'Username or email address' }).press('Tab');
+    await page.getByRole('textbox', { name: 'Password' }).fill('tester123');
+
+    //enter sign in
+    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+    await expect(page.getByText('Incorrect username or')).toHaveText('Incorrect username or password.');
+
+})
+
+test.only('Test3', async ({ page }) => {
+
+    console.log('Test Execution Started..')
+    //goto url
+    await page.goto('https://github.com/');
+
+    //click signin
+    await page.getByRole('link', { name: 'Sign in' }).click();
+
+    //enter username & password
+    await page.getByRole('textbox', { name: 'Username or email address' }).fill('testerstalk');
+    await page.getByRole('textbox', { name: 'Username or email address' }).press('Tab');
+    await page.getByRole('textbox', { name: 'Password' }).fill('tester123');
+
+    //enter sign in
+    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+    await expect(page.getByText('Incorrect username or')).toHaveText('Incorrect username or password.');
+
+})
