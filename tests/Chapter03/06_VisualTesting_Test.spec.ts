1c7a4caecd6e68f5f85bd369783d20b0fdd5cb3c
hassane08433-wq <hassane08433@gmail.com>
2026-01-31 17:30:48 +0500

    update on visual comparison

--- tests/Chapter03/06_VisualTesting_Test.spec.ts
+++ tests/Chapter03/06_VisualTesting_Test.spec.ts
@@ -7,27 +7,27 @@ import { test, expect } from '@playwright/test';
 //     await expect(page).toHaveScreenshot('gitHubloginpage.png');
 
 //    // await page.locator('[class="form-control js-login-field"]').fill('Tester123');
-//    //await page.locator('#login_field').fill('Tester123');
+//    await page.locator('#login_field').fill('Tester123');
 
 
-//     await expect(page).toHaveScreenshot('gitHubloginpage.png');
-// })
+//     await expect.soft(page).toHaveScreenshot('gitHubloginpage.png');
+// });
 
 
 test('Element comparison in Playwright', async ({ page }) => {
 
     await page.goto('https://github.com/login');
-    
+
     //const element= page.locator('[class="form-control js-login-field"]',{ name: 'Login' }, {type: 'text'});
 
-     const element = page.locator('[id="login_field"]');
+    //const element = page.locator('[id="login_field"]');
 
-   // const element = page.getByRole('textbox', { name: 'Username or email address' });
+    const element = page.getByRole('textbox', { name: 'Username or email address' });
     await expect(page).toHaveScreenshot('GitHubLoginForm.png');
 
 
     await element.fill('Tester123');
- 
+
     await expect(element).toHaveScreenshot('GitHubLoginForm.png');
 
-});
+})
