ad0662031c00f171b830f77a05c2e0308cc0a13b
hassane08433-wq <hassane08433@gmail.com>
2026-02-04 09:36:44 +0500

    popups

--- tests/Chapter04/03_AlertsPopups_Test.spec.ts
+++ tests/Chapter04/03_AlertsPopups_Test.spec.ts
@@ -1,26 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-test('Popups in Playwright Test', async ({ page }) => {
+test('Handling Popups in Playwright Test', async ({ page }) => {
     await page.goto('https://www.selenium.dev/documentation/webdriver/interactions/alerts/');
 
-    page.once('dialog', async (dialog) => {
-         console.log('dialog type is: ${dialog.type()}');
-        
-        console.log(`Alert Message: ${dialog.message()}`);
-        await dialog.accept('playwright');  
+    page.once('dialog', dialog => {
+       dialog.accept('playwright');
+        console.log(`Alert Message is: ${dialog.message()}`);
     });
 
     //await page.getByRole('link', { name: 'See an example alert', exact: true }).click({button: 'left'});
     //something wron with this page
 
     await page.click('text=See an example alert');
+    await page.getByText('See an example alert ', { exact: true }).click({ button: 'left' });
 });
 
 test('confirmPopups in Playwright Test', async ({ page }) => {
     await page.goto('https://www.selenium.dev/documentation/webdriver/interactions/alerts/');
 
     page.once('dialog', dialog => {
-         console.log('dialog type is: ${dialog.type()}');
+        console.log('dialog type is: ${dialog.type()}');
+        //await dialog.accept();
+       //dialog.dismiss is for pressing cancel button on screen
+        //and dialog.accept is for clicking ok button on screen
         dialog.dismiss();
         console.log(`Alert Message: ${dialog.message()}`);
     });
@@ -28,21 +30,24 @@ test('confirmPopups in Playwright Test', async ({ page }) => {
     await page.getByText('See a sample confirm', { exact: true }).click({ button: 'left' });
     //something wron with this page
 
-   // await page.click('text=See an example alert');
+    // await page.click('text=See an example alert');
 });
 
-test('Prompts in Playwright Test', async ({ page }) => {
+test('HandlingPrompts pop ups in Playwright Test', async ({ page }) => {
     await page.goto('https://www.selenium.dev/documentation/webdriver/interactions/alerts/');
 
-    page.once('dialog', dialog => {
-         console.log('dialog type is: ${dialog.type()}');
-        dialog.dismiss();
+    page.once('dialog', async (dialog) => {
+
+        //asyn is used here to return some text value on browser
+        
         console.log(`Alert Message: ${dialog.message()}`);
-       
+        await dialog.accept('playwright');  
+       // console.log('dialog type is: ${dialog.type()}');
+
     });
 
     await page.getByText('See a sample prompt', { exact: true }).click({ button: 'left' });
     //something wron with this page
 
-   // await page.click('text=See an example alert');
+    // await page.click('text=See an example alert');
 });
